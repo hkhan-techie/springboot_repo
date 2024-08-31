110a41703764a8d3ff7917ee0d514b379d28b243
Hidayathulla Khan <hkhan.techie@gmail.com>
2024-08-31 11:07:39 +0530

    In memory, list of employees REST API implementation.

--- rest-api-demo/src/main/java/com/technoflix/springboot/restapidemo/RestApiDemoController.java
+++ rest-api-demo/src/main/java/com/technoflix/springboot/restapidemo/RestApiDemoController.java
@@ -1,15 +1,34 @@
 package com.technoflix.springboot.restapidemo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest-api")
 public class RestApiDemoController {
+    static List<Employee> employees= new ArrayList<Employee>();
+    static {
+         employees.add(new Employee(1, "Hidayath"));
+         employees.add(new Employee(2, "John"));
+    }
+
 
     @GetMapping
     public String restGetApi() {
         return "Welcome to Rest API demo !!";
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    @PostMapping("/addEmployee")
+    public List<Employee> addEmployee(@RequestBody Employee employee) {
+        employees.add(employee);
+        return employees;
     }
 }
