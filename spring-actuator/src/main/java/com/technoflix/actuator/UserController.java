f5e00ab3ee3495bfdd2caf2ca4277a3a06227671
Hidayathulla Khan <hkhan.techie@gmail.com>
2024-09-03 06:47:50 +0530

    Spring boot actuator implementation.

---
+++ spring-actuator/src/main/java/com/technoflix/actuator/UserController.java
@@ -0,0 +1,19 @@
+package com.technoflix.actuator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    UserEntity entity;
+
+    @GetMapping("/data")
+    public UserEntity getEntity(){
+        return entity;
+    }
+}
---
+++ spring-actuator/src/main/java/com/technoflix/actuator/UserEntity.java
@@ -0,0 +1,54 @@
+package com.technoflix.actuator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserEntity {
+    private String id = "1";
+    private String name = "Hidayath";
+    private String userName = "hidayath";
+    private String email = "support@technoflix.in";
+    private String pincode = "572101";
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+
+}
